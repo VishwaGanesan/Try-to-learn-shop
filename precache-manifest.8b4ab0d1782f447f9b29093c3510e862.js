5c62e62cec288602a8ad21ded0c93ef8e4417857
vishwa.ganesan <vishwa.ganesan@zohocorp.com>
2019-08-14 22:02:29 +0530

    Updates

---
+++ precache-manifest.8b4ab0d1782f447f9b29093c3510e862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b005028d2d67174e284f39666e8879a7",
+    "url": "/Try-to-learn-shop/index.html"
+  },
+  {
+    "revision": "9dfc4d8866f45b4b7f2f",
+    "url": "/Try-to-learn-shop/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "533b74fe9bbe53e74369",
+    "url": "/Try-to-learn-shop/static/js/2.2be321c3.chunk.js"
+  },
+  {
+    "revision": "9dfc4d8866f45b4b7f2f",
+    "url": "/Try-to-learn-shop/static/js/main.01466aa8.chunk.js"
+  },
+  {
+    "revision": "454a8708a7ded682677a",
+    "url": "/Try-to-learn-shop/static/js/runtime~main.23b140c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Try-to-learn-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
